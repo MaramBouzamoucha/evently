057998fc73ccee645a6acb63eb7f142cb103cf39
Maram BOUZAMOUCHA <maram.bouz29@gmail.com>
2026-03-22 16:50:59 +0100

    correction de join

--- app/api/participations/join/route.js
+++ app/api/participations/join/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../../lib/auth";
-import prisma from "@/lib/prisma"; // <--- ASSURE-TOI QUE CET IMPORT EST CORRECT
+import prisma from "../../../lib/prisma"; // <--- ASSURE-TOI QUE CET IMPORT EST CORRECT
 
 export async function POST(req) {
   try {
